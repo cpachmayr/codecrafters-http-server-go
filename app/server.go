4b8b065300e38d44e778af8a06d0dc3aa73eff20
Coby Pachmayr <coby@ideaspring.com>
2024-05-18 17:14:41 -0700

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -6,19 +6,70 @@ import (
 	"os"
 )
 
+func handleError(msg string, err error) {
+	fmt.Printf("Encountered error:\n%s\n%v", msg, err)
+	os.Exit(1)
+}
+
+const (
+	CRLF  = "\r\n"
+	HTTPV = "HTTP/1.1"
+)
+
+type Http_Response struct {
+	Version string
+	Status  int
+	Reason  string
+	Headers string
+	Body    string
+}
+
 func main() {
+	type Http_Response struct {
+		Version string
+		Status  int
+		Reason  string
+		Headers string
+		Body    string
+	}
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+		handleError("Failed to bind to port 4221", err)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		handleError("Error accepting connection", err)
 	}
+
+	respTest := Http_Response{
+		Version: HTTPV,
+		Status:  200,
+		Reason:  "OK",
+		Headers: CRLF,
+		Body:    CRLF,
+	}
+
+	resp := fmt.Sprintf("%s %d %s\r\n%s\r\n%s", respTest.Version, respTest.Status, respTest.Reason, respTest.Headers, respTest.Body)
+
+	_, err = conn.Write([]byte(resp))
+	if err != nil {
+		handleError("Problem writing response", err)
+	}
+
+	/*
+		resp := Http_Response{
+			Version: "HTTP/1.1",
+			Status:  200,
+			Reason:  "OK",
+			Headers: CRLF,
+			Body:    CRLF,
+		}
+	*/
+	// r := fmt.Sprintf("%s %d %s%s", resp.Version, resp.Status, resp.Headers, resp.Body)
+	// conn.Write([]byte("HTTP/1.1 200 OK\r\nHello World"))
+	// fmt.Println("Response", r)
 }
